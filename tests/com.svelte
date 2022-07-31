c3b1ccde6a510ab77067995cceefad0011c655b1
Vu Nguyen <tuanvu.vn007@gmail.com>
2022-07-31 19:28:11 +0800

    add more example

---
+++ tests/com.svelte
@@ -0,0 +1,11 @@
+<p>Styled!</p>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+    background-color: var(--blue-5);
+		padding: var(--test-color)
+	}
+</style>
---
+++ tests/com.vue
@@ -0,0 +1,15 @@
+<p>Styled!</p>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+		border-color: var(--blue-5);
+		background-color: var(--test-color);
+	}
+</style>
+
+<script>
+
+</script>
